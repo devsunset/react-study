605588f7c439745dc7994d16cef743c7f94e971c
devsunset <devsunset@gmail.com>
2022-08-29 14:00:31 +0900

    dev

--- react-project/src/ComponentStyleExample.js
+++ react-project/src/ComponentStyleExample.js
@@ -32,6 +32,13 @@ const ComponentStyleExample = () => {
       <div>
         sass-loader 설정 커스터 마이징 - yarn eject (git 연동중이라면 모든거
         commit후 실행)
+        <br />
+        config/webpack.config.js 파일 assRegex 기술 부분 수정하여 scss파일
+        인식시 상대 경로 아닌 절대 경로로 인식 ex) @import "./styles/utils"; -
+        @import "utils.scss";
+        <br />
+        additionalData: "@import 'utils';", 구문 추가하면 모든 파일에 기본적으로
+        해당 내용 추가 됨
       </div>
     </div>
   );
